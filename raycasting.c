12ba7ca1d59f30e84a83fab7b744888e64c6199b
banditklm <kaltoum.elmounjid123.com>
2025-01-26 10:30:35 +0100

    clean some lines

--- raycasting.c
+++ raycasting.c
@@ -82,24 +82,3 @@ void    raytrace(t_data *data, int map_x, int map_y)
     else
         perpWallDist = data->ray.side_y - data->ray.delta_y;
 }
-
-
-//  double  ray_angle;
-//     double  segment;
-//     double  start_angle;
-//     double  raydir_x;
-//     double  raydir_y;
-//     int     i;
-//     int     screen_width;
-
-//     segment = data->player.fov / data->map_width;
-//     start_angle = atan2(data->player.y_dir, data->player.x_dir) - (data->player.fov / 2)
-//     screen_width = data->screen_width;
-//     while (i < screen_width)
-//     {
-//         ray_angle = start_angle + i * segment;
-//         raydir_x = cos(ray_angle);
-//         raydir_y = sin(ray_angle);
-//         raytrace();
-//         i++;
-//     }
